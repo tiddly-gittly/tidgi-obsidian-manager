c803925b4b23327f74de7659ddd82b92469813de
WhiteFall <1947114574@qq.com>
2023-06-03 16:32:28 +0800

    add button
    Update index.ts
    
    add button

--- src/obsidian-manager/index.ts
+++ src/obsidian-manager/index.ts
@@ -1,7 +1,7 @@
 import { IChangedTiddlers } from 'tiddlywiki';
 import { widget as Widget } from '$:/core/modules/widgets/widget.js';
 
-class ExampleWidget extends Widget {
+class ObMainWidget extends Widget {
   refresh(_changedTiddlers: IChangedTiddlers) {
     return false;
   }
@@ -10,23 +10,46 @@ class ExampleWidget extends Widget {
     this.parentDomNode = parent;
     this.execute();
     // 在这里写UI以及http请求。
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/obstore/C:/Users/Snowy/Documents/GitHub/Neural-Networks", true);
+    const xhr = new XMLHttpRequest();
+    let obDate;
     xhr.onreadystatechange = function () {
       if (this.readyState === 4 && this.status === 200) {
-        // 响应内容已准备好，可以使用 xhr.responseText 访问它
-        var a = JSON.parse(xhr.response);
-        console.log(a);
-        console.log("http请求")
       }
-    };
-    xhr.send();
-
+    }
     const containerElement = $tw.utils.domMaker('p', {
       text: 'This is a widget!',
     });
+    const addButtonElement = $tw.utils.domMaker("button", {
+      class: "my-button",
+      text: "Add",
+    });
+    const purgeButtonElement = $tw.utils.domMaker("button", {
+      class: "my-button",
+      text: "purge",
+    });
+
+    addButtonElement.onclick = function () {
+      // 加入提示，消息。
+      xhr.open("GET", "/obstore/C:/Users/Snowy/Documents/GitHub/Neural-Networks", true);
+      xhr.send();
+      obDate = JSON.parse(xhr.response);
+      for (const key in obDate.md) {
+        // 替换掉图片的符号。
+        let title = key.split(".")[0];
+        // $tw.wiki.addTiddler(new $tw.Tiddler({ title: title, type: "text/markdown", text: obDate.md[key] }));
+        console.log("创建条目：" + title);
+      }
+      // 发送请求。
+    }
+
+    purgeButtonElement.onclick = function () {
+
+    }
+
     this.domNodes.push(parent.appendChild(containerElement));
+    this.domNodes.push(parent.appendChild(addButtonElement));
+    this.domNodes.push(parent.appendChild(purgeButtonElement));
   }
 }
 
-exports.obm = ExampleWidget;
+exports.obm = ObMainWidget;
