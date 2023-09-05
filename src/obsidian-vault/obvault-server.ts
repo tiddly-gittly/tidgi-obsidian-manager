86bd73926d71ede416faf8acd7c0243bbe00c74e
WhiteFall <1947114574@qq.com>
2023-09-05 20:17:56 +0800

    Update obvault-server.ts

--- src/obsidian-vault/obvault-server.ts
+++ src/obsidian-vault/obvault-server.ts
@@ -6,23 +6,31 @@ class ObvaultServer {
 
     constructor() {
         $tw.rootWidget.addEventListener('tw-obsidian-add', async (event) => {
-            if (event.type === "tw-obsidian-add") {    
-                console.log(event.paramObject.path);
-                // 其实点几次都可以，只有一次有效。
-                if (event.paramObject.path !== '') {
-                    let data = await fetchData(event.paramObject.path, event.paramObject.reg, event.paramObject.ignore);
-                    // console.log(data);
-                    if (data != false) { addVault(data); }
-                } else {
-                    console.log("输入为空！");
-                    tm_notify("addVault", "输入为空！");
-                }
+            // 其实点几次都可以，只有一次有效。
+            if (this.isValidPath(event.paramObject.path)) {
+                let data = await fetchData(event.paramObject.path, event.paramObject.reg, event.paramObject.ignore);
+                // console.log(data);
+                if (data != false) { addVault(data); }
             }
         });
         $tw.rootWidget.addEventListener('tw-obsidian-purge', async (event) => {
             purgeVault();
         });
         $tw.rootWidget.addEventListener('tw-obsidian-update', async (event) => { });
+    }
+    isValidPath(path: string) {
+        const pathRegEx = /^(\/|\.\.?\/|([A-Za-z]:)?[\\|\/])[^\\|\/]+([\\|\/][^\\|\/]+)*[\\|\/]?$/
+        if (path === '') {
+            console.log("路径为空！");
+            tm_notify("addVault", "路径为空！");
+            return false
+        }
+        if (!pathRegEx.test(path)) {
+            console.log("无效路径！");
+            tm_notify("addVault", "无效路径！");
+            return false
+        }
+        return true
     }
 }
 
