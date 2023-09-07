f566ae9d8973b8f6dea6accefacb0f79ad2add5f
WhiteFall <1947114574@qq.com>
2023-09-07 19:42:06 +0800

    fix little bug
    fix syntax ext is arry
    
    trim 空格 修复类型识别错误

--- src/obsidian-vault/syntax/index.ts
+++ src/obsidian-vault/syntax/index.ts
@@ -84,18 +84,18 @@ function links_wiki_syntax(page_content, bp_peer, vaultname) {
 function embeds_wiki_syntax(page_content, bp_peer, vaultname) {
     page_content = page_content.replace(/\!\[\[(.*?)\]\]/g, (match, p1) => {
         const content_arr = p1.split('|');
-        const ext = content_arr[0].split('.').slice(-1);
+        const ext = content_arr[0].split('.').slice(-1)[0].trim();
         const defext = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg'];
         if (defext.indexOf(ext) !== -1) {
             // 是图片类型，有位于尾部的大小参数。
             // [img width=100 [内部链接]]
             if (content_arr.length === 1) {
-                return '[img ' + '[' + content_arr[0] + ']]';
+                return '[img ' + '[' + content_arr[0].trim() + ']]';
             }
             if (content_arr.length > 1) {
                 let size = content_arr.slice(-1)[0] * 1;
                 if (typeof size === 'number') {
-                    return '[img width=' + size + ' [' + content_arr[0] + ']]';
+                    return '[img width=' + size + ' [' + content_arr[0].trim() + ']]';
                 }
             }
         } else {
@@ -117,22 +117,22 @@ function embeds_wiki_syntax(page_content, bp_peer, vaultname) {
     page_content = page_content.replace(/\!\[(.*?)\]\((.*?)\)/g, (match, p1, p2) => {
         const content_arr = p1.split('|');
         const link = p2;
-        const ext = link.split('.').slice(-1);
+        const ext = link.split('.').slice(-1)[0].trim();
         const defext = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg', 'ico'];
         if (defext.indexOf(ext) !== -1) {
             // [img [Motovun Jack.jpg]]
-            if (content_arr.length === 0 || content_arr[0] === '') {
+            if (content_arr.length === 0 || content_arr[0].trim() === '') {
                 return '[img ' + '[' + link + ']]';
             }
             if (content_arr.length === 1) {
-                return '[img ' + '[' + content_arr[0] + '|' + link + ']]';
+                return '[img ' + '[' + content_arr[0].trim() + '|' + link + ']]';
             }
             // ![AltText|100x100](https://url/to/image.png)
             // [img width=32 [Motovun Jack|Motovun Jack.jpg]]
             if (content_arr.length > 1) {
                 let size = content_arr.slice(-1)[0].split('x')[0] * 1;
                 if (typeof size === 'number') {
-                    return '[img width=' + size + ' [' + content_arr[0] + '|' + link + ']]';
+                    return '[img width=' + size + ' [' + content_arr[0].trim() + '|' + link + ']]';
                 }
             }
         } else {
