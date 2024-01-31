4ce8671e7701934f50dbff8c3b6be3f5b09907c1
Zacharia2 <32425955+Zacharia2@users.noreply.github.com>
2024-01-31 19:45:29 +0800

    use rc_tree

--- src/obsidian-vault/react-tree/tree-widget.tsx
+++ src/obsidian-vault/react-tree/tree-widget.tsx
@@ -1,9 +1,8 @@
 import { IChangedTiddlers } from 'tiddlywiki';
 import { widget as Widget } from '$:/core/modules/widgets/widget.js';
 
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
-// import { type IDefaultWidgetProps, ParentWidgetContext } from '$:/plugins/linonetwo/tw-react/index.js';
+import React from 'react';
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
 
@@ -16,19 +15,17 @@ class TreeWidget extends Widget {
     return false;
   }
 
-  async render(parent:any, _nextSibling:any) {
+  async render(parent: any, _nextSibling: any) {
     this.parentDomNode = parent;
     this.execute();
     // 如何在这里使用CSS呢？
     const containerElement = $tw.utils.domMaker('div', {
       text: 'This is a tree widget!',
     });
-    const root = ReactDOMClient.createRoot(containerElement);
-    // root.render(
-    //   <StrictMode>
-    //     <App />
-    //   </StrictMode>
-    // );
+    // const rootElement = document.getElementById("root");
+    const root = createRoot(containerElement);
+
+    root.render(<App />);
     this.parentDomNode.append(containerElement)
   }
 }
